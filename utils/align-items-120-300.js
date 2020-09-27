1a5ac5c55a9fb5d4f9f923842eb0672674ebe88a
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-09-27 19:03:15 +0200

    Adds utils for algning inventory (patch)

---
+++ utils/align-items-120-300.js
@@ -0,0 +1,17 @@
+(async () => {
+  const { writeFile } = require('fs').promises
+  const alignment = require('./b5-300-120-alignment.json')
+  const language = 'en'
+  const questions = require(`../data/${language}/questions.json`)
+  const getAlignment = id => alignment.find(item => item['300'] === id)
+  const alignedQuestions = questions.reduce((accumulator, current) => {
+    const aligned = getAlignment(current.id)
+    if (aligned) {
+      current.id = aligned['120']
+    }
+    accumulator.push(current)
+    return accumulator
+  }, [])
+  await writeFile(`data/${language}/questions.json`, JSON.stringify(alignedQuestions, null, 2), 'utf-8')
+  console.log(`Aligned questions for ${language}`)
+})()
